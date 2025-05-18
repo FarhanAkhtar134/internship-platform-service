5ef72fbc219ef809ed1e603576bf67ed99bbc4fe
farhan <farhanakhtardx2@gmail.com>
2025-05-18 11:57:40 +0800

    Added initial files

---
+++ src/main/java/com/moby/internshipplatform/InternshipplafromApplication.java
@@ -0,0 +1,13 @@
+package com.moby.internshipplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipplafromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipplafromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moby/internshipplatform/InternshipplafromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moby.internshipplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipplafromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
